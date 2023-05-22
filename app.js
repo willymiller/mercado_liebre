d06332c7059455c7c7173a98b79ec2b82576e36b
willymiller <65316037+willymiller@users.noreply.github.com>
2023-05-22 13:40:33 -0300

    Login, Register y ProductCreate

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const path = require("path");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) =>{
-    res.sendFile(path.join(__dirname, "./views/home.html"))
-});
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+})
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+})
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+})
+
+app.get('/productCreate', (req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/productCreate.html'))
+})
 
 app.listen(3000, () =>{
     console.log("Servidor corriendo en el puerto 3000")
